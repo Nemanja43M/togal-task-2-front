c0825545d73fd2a99fdb4da4ba03cff3388abd9e
Nemanja Markovic <nemanjaamarkovic91@gmail.com>
2024-10-31 08:08:19 +0100

    Minor ajustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import "./App.css";
 import ImageUploader from "./components/ImageUploader";
-import { Sidebar } from "./components/SideBar";
 import { StyledBackground } from "./components/style/styled";
 
 function App() {
   return (
     <StyledBackground>
-      <Sidebar />
       <ImageUploader />
     </StyledBackground>
   );
